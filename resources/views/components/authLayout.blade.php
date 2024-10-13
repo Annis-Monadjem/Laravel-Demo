dee4e35acef0c11424a11f975a3901262ce61215
Annis-Monadjem <13934141+Annis-Monadjem@users.noreply.github.com>
2024-10-14 00:05:38 +0200

    Develope a simple page layout for user authentication

--- resources/views/components/authLayout.blade.php
+++ resources/views/components/authLayout.blade.php
@@ -6,16 +6,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="shortcut icon" sizes="16x16" type="image/x-icon" href="{{ asset('favicon16x16.png') }}">
-        <link rel="shortcut icon" sizes="32x32" type="image/x-icon" href="{{ asset('favicon32x32.png') }}">
-        <link rel="shortcut icon" sizes="64x64" type="image/x-icon" href="{{ asset('favicon64x64.png') }}">
-        @vite(['resources/css/app.css', 'resources/css/normalize.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         <title>Fakeface</title>
     </head>
 
     <body>
-        <header></header>
-        {{ $slot }}
-        <footer></footer>
+        <div class="container">
+            <header></header>
+            <main>
+                {{ $slot }}
+            </main>
+            <footer></footer>
+        </div>
     </body>
 
 </html>
