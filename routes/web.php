c5f8e9f5da4acefb92d7591929defd3643345c80
Annis-Monadjem <13934141+Annis-Monadjem@users.noreply.github.com>
2024-10-21 20:36:49 +0200

    Implement login public site
    Connect login route to UserController login method

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [UserController::class, "login"])->name("login");
+Route::get('/register', function () {
+    return view("showRegistrationForm");
+});
+Route::post('/register', function () {
+    return view("showRegistrationForm");
+});
+Route::get('/loggedHomepage', function () {
+    return view("loggedHome");
 });
