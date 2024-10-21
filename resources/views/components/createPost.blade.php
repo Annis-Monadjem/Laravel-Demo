957a38b29b68fa2aa84751c6c8713b1d74e2c198
Annis-Monadjem <13934141+Annis-Monadjem@users.noreply.github.com>
2024-10-21 15:35:28 +0200

    Create "createPost" post component blade template

---
+++ resources/views/components/createPost.blade.php
@@ -0,0 +1,10 @@
+<div class="create-post" id="create-post">
+    <div class="post">
+        <form action="" method="post">
+            <h1>Create post</h1>
+            <a href="#section">x</a>
+            <textarea placeholder="What's on your mind?"></textarea>
+            <button>Add to your post</button>
+        </form>
+    </div>
+</div>
