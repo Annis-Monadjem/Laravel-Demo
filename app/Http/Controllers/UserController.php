6a2f8513cbb89bb7ececa4a353a4028a78cb291b
Annis-Monadjem <13934141+Annis-Monadjem@users.noreply.github.com>
2024-10-22 12:32:34 +0200

    Create submit registration form and show main page logic

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function login()
+    public function showLogin()
     {
         return view("welcome");
+    }
+    public function submitLogin(Request $request)
+    {
+        $userInput = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        if (Auth::attempt(['emailaddress' => $userInput['email'], 'password' => $userInput['password']])) {
+            $request->session()->regenerate();
+        }
+        return redirect()->route('loggedHome');
     }
     public function showRegistrationForm()
     {
         return view("showRegistrationForm");
+    }
+    public function submitRegistrationForm(Request $request)
+    {
+        $userInput = $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'birthdate' => ['required', 'date'],
+            'gender' => 'required',
+            'emailaddress' => ['required', 'email', Rule::unique('users', 'emailaddress')],
+            'password' => ['required', 'min:8']
+        ]);
+
+        try {
+            $userInput['firstname'] = strip_tags($userInput['firstname']);
+            $userInput['lastname'] = strip_tags($userInput['lastname']);
+            $userInput['birthdate'] = strip_tags($userInput['birthdate']);
+            $userInput['emailaddress'] = strip_tags($userInput['emailaddress']);
+            $userInput['password'] = strip_tags($userInput['password']);
+
+            $userInput['password'] = bcrypt($userInput['password']);
+
+            $user = User::create($userInput);
+            if ($user) {
+                Auth::login($user);
+                return redirect()->route("loggedHome");
+            }
+            return redirect()->route("publicSite");
+        } catch (\Exception $e) {
+            return back()->withErrors(['msg' => 'Failed to register user. Please try again.']);
+        }
+    }
+    public function showMainPage()
+    {
+        if (!Auth::check()) return redirect()->route('publicSite');
+        return view('loggedHome');
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,9 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', [UserController::class, "login"])->name("login");
 Route::get("/register", [UserController::class, "showRegistrationForm"]);
+Route::post("/register", [UserController::class, "submitRegistrationForm"]);
+Route::get("/loggedHome", [UserController::class, "showMainPage"])->name('loggedHome');
+Route::get('/', [UserController::class, "showLogin"])->name('publicSite');
+Route::post('/', [UserController::class, "submitLogin"]);
+Route::post('/logout', [UserController::class, "logout"]);
